33371691f451fdfbf1a07f3ac56bff81fc8a4c28
Dzintars <d.krakops@t-online.de>
2026-06-15 20:45:56 +0200

    src v 2

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,13 @@
 import styles from "./styles.module.css";
 import Header from "../../components/header";
+import CategoryCardList from "../../components/categoryCardList";
 
 function Home() {
   return (
     <div className={styles.homeContainer}>
       <Header />
+      {/* Main */}
+      {/* Footer */}
     </div>
   );
 }
